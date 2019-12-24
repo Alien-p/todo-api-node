b538818e988049b4462926db1c99d3003b76e0c1
Ildar Gumerov <Ildar.Gumerov@icl-services.com>
2019-12-24 17:33:17 +0300

    Complete routes fot todo api

--- app.js
+++ app.js
@@ -3,18 +3,22 @@
  */
 const express = require("express");
 const todoRoutes = require("./routes/todos")
+const bodyParser = require("body-parser");
+
 
 /**
  * App variables
  */
 const app = express();
 const PORT = process.env.PORT || "8000";
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 /**
  * Routes
  */
 app.get("/", (req, res) => {
-    res.status(200).send("All works fine");
+  res.status(200).send("All works fine");
 });
 
 app.use("/api/todos", todoRoutes);
